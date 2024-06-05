67cd54fc0412e3b09678411b2c273ff45dd5d2bb
MohamedElb17 <mohamed.elbidaoui2003@gmail.com>
2024-06-06 00:01:16 +0200

    Gebruik van Arrays voor products

---
+++ products.js
@@ -0,0 +1,13 @@
+export const products = [
+    { id: 1, name: 'Red Dead Redemption II PS4', price: 19.99 },
+    { id: 2, name: 'Assassins Creed II PS3', price: 29.99 },
+    { id: 3, name: 'Ghost of Tsushima PS5', price: 39.99 },
+    { id: 4, name: 'God of War PS3', price: 49.99 },
+    { id: 5, name: 'The Last of Us PS4', price: 9.99 },
+    { id: 6, name: 'Days Gone PS4', price: 15.99 },
+    { id: 7, name: 'Watch Dogs PS3', price: 24.99 },
+    { id: 8, name: 'The Witcher PS3', price: 34.99 },
+    { id: 9, name: 'Batman Arkham Ayslum PS3', price: 44.99 },
+    { id: 10, name: 'Elden Ring PS5', price: 9.99 },
+    
+];
