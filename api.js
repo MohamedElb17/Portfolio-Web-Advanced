cbc6ab8f44219a691105282e80759225e4407095
MohamedElb17 <mohamed.elbidaoui2003@gmail.com>
2024-06-06 05:35:21 +0200

    Localstorage

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ export async function fetchProducts() {
                 { id: 3, name: 'Ghost of Tsushima PS5', price: 39.99 },
                 { id: 4, name: 'God of War PS3', price: 49.99 },
                 { id: 5, name: 'The Last of Us PS4', price: 9.99 },
-                { id: 6, name: 'Days Gone PS4', price: 15.99 },
+                { id: 6, name: 'CyberPunk 2077 PS4', price: 15.99 },
                 { id: 7, name: 'Watch Dogs PS3', price: 24.99 },
                 { id: 8, name: 'The Witcher PS3', price: 34.99 },
                 { id: 9, name: 'Batman Arkham Ayslum PS3', price: 44.99 },
